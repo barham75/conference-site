acaac96b3efbb7e131e8c5b6b3de4bd08f53cba4
Mohammad Barham <mah.barham04777@gmail.com>
2026-02-07 21:41:35 +0300

    Redirect /vote to /poster-vote

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: "/vote",
+        destination: "/poster-vote",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
